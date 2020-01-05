e6f602389eccdbd394fab025148ed45146e0aadc
herohero1022 <eq646261@gmail.com>
2020-01-05 17:22:49 +0900

    修正

--- resources/views/recipe/new.blade.php
+++ resources/views/recipe/new.blade.php
@@ -6,25 +6,9 @@
       </title>
   </head>
   <body>
-    <div class="home-wrapper">
-      <div class="home-header">
-        <div class="header-contents">
-          <div class="header-contents-top">
-            <h2 class="header-contents-top-title">RecipeBock</h2>
-            <div class="header-contents-top-serch">
-              レシピを検索
-              <i class="fas fa-search"></i>
-            </div>
-          </div>
-          <div class="header-contents-bottom">
-            <div class="header-contents-bottom-regibtn">
-              新規会員登録
-            </div>
-            <div class="header-contents-bottom-loginbtn">
-              ログイン
-            </div>
-          </div>
-        </div>
+    <div class="recipe-new-wrapper">
+      <div class="recipe-header">
+        Recipebook
       </div>
       <div class="add-main-contents">
         <div class="add-content">
