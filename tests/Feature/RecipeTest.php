d57c818feb472e52b0d7f7371d0f3d106a007a1b
herohero1022 <eq646261@gmail.com>
2020-01-11 23:34:03 +0900

    一時保存

--- tests/Feature/RecipeTest.php
+++ tests/Feature/RecipeTest.php
@@ -10,7 +10,7 @@ use Faker\Generator as Faker;
 
 class RecipeTest extends TestCase
 {
-    // use RefreshDatabase;
+    use RefreshDatabase;
 
     /**
      * A basic feature test example.
@@ -34,14 +34,30 @@ class RecipeTest extends TestCase
     public function testCreate()
     {
         $user = factory(User::class)->create();
-        $response = $this->post('/recipe/store', [
-            'user_id' => '$user->id',
-            'title' => 'aaaa',
-            'image' => 'aaaa',
-            'description' => 'aaaa',
-            'status' => 'open'
-        ]);
+        // $response = $this->post('/recipe/store', [
+        //     'user_id' => '$user->id',
+        //     'title' => 'aaaa',
+        //     'image' => 'aaaa',
+        //     'description' => 'aaaa',
+        //     'status' => 'open'
+        // ]);
         $response->assertRedirect('/material/new/1');
+    }
+
+    public function testPreview()
+    {
+        $user = factory(User::class)->create();
+        $response = $this->actingAs($user)->get('recipe/preview');
+        $response->assertStatus(200);
+    }
+
+    public function testSearch()
+    {
+        $user = factory(User::class)->create();
+        $response = $this->actingAs($user)->get('search/index', [
+            'keyword' => 'コロッケ'
+        ]);
+        $response->assertStatus(200);
     }
 
 }
